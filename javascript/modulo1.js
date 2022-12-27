8cc12a4a76a5bbba9df189735a76a6830b7eeef0
Umbra DB <umbradb@blackhole.Home>
2022-12-27 01:17:26 +0000

    added javascript

---
+++ javascript/modulo1.js
@@ -0,0 +1,15 @@
+// breve descrição dos comandos, objectos e metodos aprendidos (/*isto é um comentário tambem*/)
+console.log("hello world"); // escreve na consola "hello world"
+/* tipos de dados:
+ *  strings (conjuntos de caracteres em valor matemático (texto))
+ *  numbers (numeros, inteiros, decimais, com valor matemático)
+ *  booleans (tem um de dois valores, verdadeiro ou falso, 0 ou 1)
+ *  null (um objecto sem valor, pode ser utilizado quando não se pretende que haja retorno)
+ *  undefined (semelhante ao null, no entanto refere que o valor dado não existe)
+ *  symbol (referido mas não explicado)
+ *  object (conjunto de dados relacionados entre si)
+ */
+console.log(Math.floor(Math.random()*100)+Math.ceil(Math.random()*2)); //escreve na console a soma de um numero de 0 a 100 arrendondado para baixo com um numero de 0 a 2 arredondado para cima)
+console.log("Isto retorna quantos caracteres aqui estão escritos".length); // o "." é utilizado para chamar métodos 
+console.log("esta frase aparece em maiúsculas".toUpperCase());
+console.log(Number.isInteger(2017)) // escreve na consola um valor booleano de acordo se o valor inserido é um inteiro ou não
